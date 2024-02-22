87604de5e37a8b542caf32a8f488db4f8a685be2
Jojo <jonas.uliana@pm.me>
2024-02-21 23:43:57 -0300

    chore(py-adb): add more tests

--- py-adb/tests/test_adb.py
+++ py-adb/tests/test_adb.py
@@ -1,10 +1,41 @@
 from unittest import TestCase
+from unittest.mock import MagicMock, patch
 
 from py_adb import Adb
-from py_adb.exceptions import AdbIsNotAvailable
+from py_adb.exceptions import AdbHaveMultipleMatches, AdbIsNotAvailable
 
 
 class TestAdb(TestCase):
-    def test_instance_creation(self):
+    @patch('py_adb.Adb.is_adb_available')
+    def test_instance_creation_without_adb(
+        self, mock_is_adb_available: MagicMock
+    ) -> None:
+        mock_is_adb_available.return_value = False
+
         with self.assertRaises(AdbIsNotAvailable):
             _ = Adb()
+
+    @patch('py_adb.Adb.discover_from_path')
+    @patch('py_adb.Adb.is_adb_available')
+    def test_instance_creation_with_multiple_entries(
+        self,
+        mock_is_adb_available: MagicMock,
+        mock_discover_from_path: MagicMock,
+    ) -> None:
+        mock_is_adb_available.return_value = True
+        mock_discover_from_path.return_value = ['1', '2']
+
+        with self.assertRaises(AdbHaveMultipleMatches):
+            _ = Adb()
+
+    @patch('py_adb.Adb.discover_from_path')
+    @patch('py_adb.Adb.is_adb_available')
+    def test_instance_creation_with_adb(
+        self,
+        mock_is_adb_available: MagicMock,
+        mock_discover_from_path: MagicMock,
+    ) -> None:
+        mock_is_adb_available.return_value = True
+        mock_discover_from_path.return_value = ['1']
+
+        _ = Adb()
